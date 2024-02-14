557a28f2e09868c5b7c22ab26a73002933c8110a
honeydbio <info+honeydb@deceptionlogic.com>
2024-02-14 08:44:40 -0500

    modified to list services

--- example3.py
+++ example3.py
@@ -10,16 +10,5 @@ honeydb = api.Client(API_ID, API_KEY)
 
 TOP_COUNT = 10
 
-for host in honeydb.twitter_threat_feed():
-    if int(host["count"]) > TOP_COUNT:
-        print(
-            "Remote host: {}\tLast seen: {}".format(
-                host["remote_host"], host["last_seen"]
-            )
-        )
-        for tweet in honeydb.twitter_threat_feed(ipaddress=host["remote_host"]):
-            print(
-                "\tUser: {}\tTweet: {}".format(
-                    tweet["screen_name"], tweet["tweet_text"]
-                )
-            )
+for service in honeydb.services():
+    print(f"{service['service']} ({service['count']})")
