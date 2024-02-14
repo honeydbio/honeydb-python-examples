712bc861982ce805747134b1859b96f7bdb16d68
honeydbio <info+honeydb@deceptionlogic.com>
2024-02-14 08:39:32 -0500

    formatting; added MIN_COUNT

--- example2.py
+++ example2.py
@@ -5,13 +5,12 @@ from honeydb import api
 
 API_ID = os.environ["HONEYDB_API_ID"]
 API_KEY = os.environ["HONEYDB_API_KEY"]
+MIN_COUNT = 10
 
 honeydb = api.Client(API_ID, API_KEY)
 
 for badhost in honeydb.bad_hosts():
-    if int(badhost["count"]) > 5:
+    if int(badhost["count"]) >= MIN_COUNT:
         print(
-            "Remote host: {}\tLast seen: {}".format(
-                badhost["remote_host"], badhost["last_seen"]
-            )
+            f"Remote host: {badhost['remote_host']}\tLast seen: {badhost['last_seen']}"
         )
